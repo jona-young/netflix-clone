2377eb85384eb5810c116aab639619a21a3097d4
jonayoung <young.jon@icloud.com>
2020-08-01 00:42:13 -0400

    connected to TMDB API, added movie rows, movie banner, css effects, etc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Row from "./Row.js";
+import requests from "./requests.js";
+import Banner from "./Banner.js";
+import Nav from "./Nav.js";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hey Jona! Let's build a Netflix Clone today!</h1>
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+      <Row title="Top Rated" fetchURL={requests.fetchTrending} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
     </div>
   );
 }
