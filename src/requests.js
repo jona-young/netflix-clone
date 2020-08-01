63885b3e0110cdec012baea1f1775dffa31ceb7c
jonayoung <young.jon@icloud.com>
2020-08-01 18:17:09 -0400

    fixed requests typo

--- src/requests.js
+++ src/requests.js
@@ -1,14 +1,14 @@
-const APIKEY = '9530f687759905600eef6cf3f11788e0';
+const APIKEY = "9530f687759905600eef6cf3f11788e0";
 
 const requests = {
-    fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with_networks=213`,
-    fetchTopRated: `/movie/top_rated?api_key=${APIKEY}}&language=en-US`,
-    fetchActionMovies: `/discover/movie?api_key=${APIKEY}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${APIKEY}&with_genres=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${APIKEY}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${APIKEY}&with_genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${APIKEY}&with_genres=99`,
+  fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${APIKEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${APIKEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${APIKEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${APIKEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${APIKEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${APIKEY}&with_genres=99`,
 };
 
 export default requests;
