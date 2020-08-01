50fbce0127ee9b741f4804f3be866e5238485ce4
jonayoung <young.jon@icloud.com>
2020-08-01 18:11:01 -0400

    Updates

---
+++ precache-manifest.8a3c6215ba836764efeda1e2a60d622a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5eb5d90439b90d4f36e2ffc7df7fed7",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "995da8311f6def2dac6b",
+    "url": "/netflix-clone/static/css/main.96852ab8.chunk.css"
+  },
+  {
+    "revision": "05c29ffdd38634752d66",
+    "url": "/netflix-clone/static/js/2.8a98fe2b.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.8a98fe2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "995da8311f6def2dac6b",
+    "url": "/netflix-clone/static/js/main.8eda2539.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
